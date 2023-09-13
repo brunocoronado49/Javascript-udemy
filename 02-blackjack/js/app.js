cde951680bcefa4b27e25e8d59fe958dc16f9347
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-13 11:12:44 -0600

    agregando cartas

--- 02-blackjack/js/app.js
+++ 02-blackjack/js/app.js
@@ -1,1 +1,65 @@
+const btnPedir = document.querySelector('#btnPedir');
+const btnNuevo = document.querySelector('#btnNuevo');
+const btnDetener = document.querySelector('#btnDetener');
+const puntosHTML = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas');
+
 let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'K', 'Q', 'J'];
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+// creando baraja nueva
+const crearDeck = () => {
+    // inrgeso de tipos de cartas comunes
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+    
+    // ingreso de tipos de cartas especiales
+    for(let tipo of tipos) {
+        for (let especial of especiales) {
+            deck.push(especial + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
+}
+crearDeck();
+
+// tomando una carta del final de la baraja
+const pedirCarta = () => {
+    if (deck.length === 0) throw 'No hay cartas en el deck';
+    const carta = deck.pop();
+    return carta;
+}
+pedirCarta();
+
+// darle valor a las cartas
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    // revisa si es un numero y si no lo convierte a number
+    return (isNaN(valor)) ? (valor === 'A') ? 11 : 10 : valor * 1;
+
+}
+valorCarta('4C');
+
+
+// eventos del html
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+    puntosJugador += valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+});
+
+
