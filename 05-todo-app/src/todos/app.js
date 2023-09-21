15ab243c4c01ba019575f172093922024c4524f2
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-20 21:25:39 -0600

    creando el cascaron de la app

---
+++ 05-todo-app/src/todos/app.js
@@ -0,0 +1,17 @@
+import html from './app.html?raw'
+
+/**
+ * 
+ * @param {String} elementId 
+ */
+export const App = (elementId) => {
+    // cuando la funcion se manda a llamar
+    (() => {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+
+        document.querySelector(elementId).append(app);
+    })();
+}
+
+
