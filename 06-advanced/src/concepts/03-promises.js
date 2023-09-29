84d86f715bedd402ff81224a303b857acfe2c096
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-29 11:24:27 -0600

    como utilizar una promesa

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -5,10 +5,23 @@ import { heroes } from '../data/heroes'
  * @param {HTMLDivElement} element 
  */
 export const promisesComponent = (element) => {
-    console.log('promisesComponent');
+    const renderHero = hero => {
+        element.innerHTML = hero.name;
+    }
+
+    const renderError = error => {
+        element.innerHTML = `
+            <h3>${error}</h3>
+        `; 
+    }
+
+    const id1 = '5d86371f9f80b591f499df32';
+    findHero(id1)
+        .then(captain => renderHero(captain))
+        .catch(renderError);
 }
 
-/**
+/** 
  * 
  * @param {String} id
  * @returns {Promise<Object>}
