9749b6f8b4c6af00e726e2410eea259d839b09ad
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-30 17:37:06 -0600

    promise hell y promise all

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -15,10 +15,46 @@ export const promisesComponent = (element) => {
         `; 
     }
 
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `
+            <h3>${hero1.name}</h3>
+            <h3>${hero2.name}</h3>
+        `;
+    }
+ 
     const id1 = '5d86371f9f80b591f499df32';
-    findHero(id1)
-        .then(captain => renderHero(captain))
-        .catch(renderError);
+    const id2 = '5d86371fd55e2e2a30fe1ccb';
+
+    let hero1, hero2;
+
+    //? Forma 3
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ]).then(
+        ([hero1, hero2]) => renderTwoHeroes(hero1, hero2)
+    ).catch(renderError);
+
+    //? Forma 2
+    // findHero(id1)
+    //     .then(hero => {
+    //         hero1 = hero;
+    //         return findHero(id2);
+    //     }).then(hero => {
+    //         hero2 = hero;
+    //         renderTwoHeroes(hero1, hero2);
+    //     }).catch(renderError);
+
+    //? Forma 1
+    // findHero(id1)
+    //     .then((hero1) => {
+    //         findHero(id2)
+    //             .then((hero2) => {
+    //                 renderTwoHeroes(hero1, hero2);
+    //             })
+    //             .catch(renderError);
+    //     })
+    //     .catch(renderError);
 }
 
 /** 
