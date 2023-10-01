99ab80e8766ec86c7300d7ce476d8ae80fd7aae3
brunocoronado49 <francorangelcoronado@gmail.com>
2023-10-01 15:49:21 -0600

    optimizar promesas no secuenciales y errores en async await

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -7,6 +7,7 @@ import { promisesComponent } from './src/concepts/03-promises'
 import { promiseRaceComponent } from './src/concepts/04-promise-race'
 import { asyncComponent } from './src/concepts/05-async'
 import { asyncAwaitComponent } from './src/concepts/06-async-await'
+import { asyncAwaitTwoComponent } from './src/concepts/07-async-await'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -28,6 +29,7 @@ const element = document.querySelector('.card');
 // promisesComponent(element);
 // promiseRaceComponent(element);
 // asyncComponent(element);
-asyncAwaitComponent(element);
+// asyncAwaitComponent(element);
+asyncAwaitTwoComponent(element);
 
 
