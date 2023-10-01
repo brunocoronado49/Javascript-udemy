bca490d6abebc6d43ad3a1b5ed581b94adeb23c7
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-30 19:15:35 -0600

    manejo de errores en async y await

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -8,12 +8,14 @@ export const asyncAwaitComponent = async (element) => {
     const id1 = '5d86371fd55e2e2a30fe1ccb1';
     const id2 = '5d86371f233c9f2425f16916';
 
-    const {name: name1} = await findHero(id1);
-    const {name: name2} = await findHero(id2);
-
-    element.innerHTML = `${name1} / ${name2}`;
-
-
+    try {
+        const {name: name1} = await findHero(id1);
+        const {name: name2} = await findHero(id2);
+    
+        element.innerHTML = `${name1} / ${name2}`;
+    } catch (error) {
+        element.innerHTML = error;
+    }
 }
 
 /**
