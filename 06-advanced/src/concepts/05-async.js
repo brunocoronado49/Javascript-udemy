ba338cfe85981cbd6e2757d03433978800605224
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-30 19:12:07 -0600

    async y await

---
+++ 06-advanced/src/concepts/05-async.js
@@ -0,0 +1,31 @@
+import { heroes } from '../data/heroes'
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncComponent = (element) => {
+    const id = '5d86371f2343e37870b91ef1';
+    
+    console.log('Start component');
+    findHero(id)
+        .then(name => element.innerHTML = name)
+        .catch(error => element.innerHTML = error);
+    
+    console.log('End component');
+}
+
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<String>}
+ */
+const findHero = async (id) => {
+    const hero = heroes.find(hero => hero.id === id);
+    if (!hero) throw `Hero with id ${id} not found.`;
+    
+    return hero.name;
+}
+
+
+
---
+++ 06-advanced/src/concepts/06-async-await.js
@@ -0,0 +1,31 @@
+import { heroes } from '../data/heroes'
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncAwaitComponent = async (element) => {
+    const id1 = '5d86371fd55e2e2a30fe1ccb1';
+    const id2 = '5d86371f233c9f2425f16916';
+
+    const {name: name1} = await findHero(id1);
+    const {name: name2} = await findHero(id2);
+
+    element.innerHTML = `${name1} / ${name2}`;
+
+
+}
+
+/**
+ * 
+ * @param {String} id 
+ */
+const findHero = async (id) => {
+    const hero = heroes.find(hero => hero.id === id);
+    if (!hero) throw 'Hero not found.';
+
+    return hero;
+}
+
+
+
