506951a7f0fc849eecb9dc07135fa958a548accb
brunocoronado49 <francorangelcoronado@gmail.com>
2023-10-01 16:56:47 -0600

    for await

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -8,6 +8,7 @@ import { promiseRaceComponent } from './src/concepts/04-promise-race'
 import { asyncComponent } from './src/concepts/05-async'
 import { asyncAwaitComponent } from './src/concepts/06-async-await'
 import { asyncAwaitTwoComponent } from './src/concepts/07-async-await'
+import { forAwaitComponent } from './src/concepts/08-for-await'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -30,6 +31,7 @@ const element = document.querySelector('.card');
 // promiseRaceComponent(element);
 // asyncComponent(element);
 // asyncAwaitComponent(element);
-asyncAwaitTwoComponent(element);
+// asyncAwaitTwoComponent(element);
+forAwaitComponent(element);
 
 
