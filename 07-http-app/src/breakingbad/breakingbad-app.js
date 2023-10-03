f623765b165b15c5e7be3de0f3a2261f1d74a62c
brunocoronado49 <francorangelcoronado@gmail.com>
2023-10-03 11:14:22 -0600

    breakingbad app

--- 07-http-app/src/breakingbad/breakingbad-app.js
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -1,9 +1,38 @@
 /**
  * 
+ * @returns {Object} Quote information
+ */
+const fetchQuote = async() => {
+    const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes/');
+    const data = await response.json();
+    return data[0];
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async(element) => {
     document.querySelector('#app-title').innerHTML = 'BreakingBad App';
-    element.innerHTML = 'Loading...';
+    
+    const buttonQuote = document.createElement('button');
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+
+    buttonQuote.innerText = 'Next Quote';
+
+    const renderQuote = ({quote, author}) => {
+        quoteLabel.innerHTML = quote;
+        authorLabel.innerHTML = author;
+        element.replaceChildren(quoteLabel, authorLabel, buttonQuote);
+    }
+    
+    buttonQuote.addEventListener('click', async() => {
+        element.innerHTML = 'Loading...';
+        const quote = await fetchQuote();
+        renderQuote(quote);
+    });
+    
+    fetchQuote().then(renderQuote);
 }
 
