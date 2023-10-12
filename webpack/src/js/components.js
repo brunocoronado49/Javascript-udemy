a9ea023929ed54006891991b209c4d67753f28f5
brunocoronado49 <francorangelcoronado@gmail.com>
2023-10-12 16:34:16 -0600

    instalando webpack para compatibilidad de js con navegadores

--- webpack/src/js/components.js
+++ webpack/src/js/components.js
@@ -1,7 +1,7 @@
 import webpacklogo from '../assets/img/webpacklogo.png'
 import '../css/components.css'
 
-export const saludar = (nombre) => {
+export const saludar = (nombre = 'Sin nombre') => {
     console.log('Creando etiqueta h1');
 
     const h1 = document.createElement('h1');
